b18902a7bf84bff25bc5fbbb06bdc4dc05d6dd48
Louise <loui59f1@stud.kea.dk>
2021-02-08 14:55:51 +0100

    Exercise files

---
+++ basics.js
@@ -0,0 +1,4 @@
+console.log("Hello");
+
+const number = 41;
+const string = "This is a string";
---
+++ strings.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const name = "Albus Percival Wulfric Brian Dumbledore";
+const length = name.length;
+const firstLetter = name[0];
+const secondLetter = name[1];
+const thirdLetter = name[2];
+const fourthLetter = name[3];
+const fifthLetter = name[4];
+const sixthLetter = name[5];
+const seventhLetter = name[6];
+
+console.log(
+  `${name} is ${length} characters long. The character at index 2 is ${thirdLetter} and at the 6th is ${seventhLetter}`
+);
+
+const str1 = "   There is    space here    \n   ";
+const str2 = str1.trim();
+
+console.log(str2);
