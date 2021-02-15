156a364a32eaeaa6176d1525a3b96025d0bbe22c
Louise <loui59f1@stud.kea.dk>
2021-02-15 10:41:03 +0100

    Nameparts exercises

--- nameparts.js
+++ nameparts.js
@@ -1,14 +1,33 @@
 "use strict";
 
-const name = "Louise Storegaard Nielsen";
+function getNameParts(fullName) {
+  fullName = "Louise Storegaard Nielsen";
 
-const firstSpace = name.indexOf(" ");
-const lastSpace = name.lastIndexOf(" ");
+  const firstSpace = fullName.indexOf(" ");
+  const lastSpace = fullName.lastIndexOf(" ");
 
-const firstName = name.substring(0, firstSpace);
-const middleName = name.substring(firstSpace, lastSpace).trim();
-const lastName = name.substring(lastSpace).trim();
+  const firstName = fullName.substring(0, firstSpace);
+  const middleName = fullName.substring(firstSpace, lastSpace).trim();
+  const lastName = fullName.substring(lastSpace).trim();
 
-console.log(
-  `The firstname is ${firstName}, the middlename is ${middleName} and the lastname is ${lastName}`
-);
+  console.log(
+    `The firstname is ${firstName}, the middlename is ${middleName} and the lastname is ${lastName}`
+  );
+}
+
+getNameParts();
+
+function fullName(lastName, firstName, middleName) {
+  console.log(
+    `Hello my name is ${lastName} and my firstname is ${firstName} and ${middleName}`
+  );
+
+  const firstSpace = fullName.indexOf(" ");
+  const lastSpace = fullName.lastIndexOf(" ");
+
+  firstName = fullName.substring(0, firstSpace);
+  middleName = fullName.substring(firstSpace, lastSpace).trim();
+  lastName = fullName.substring(lastSpace).trim();
+}
+
+fullName("Potter", "Harry", "James");
