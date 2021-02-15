f3625e0a25ca6fa5943ed450160ce954c34476fd
Louise <loui59f1@stud.kea.dk>
2021-02-15 10:22:53 +0100

    Functions with parameters

--- capitalization.js
+++ capitalization.js
@@ -1,7 +1,11 @@
 "use strict";
 
-const name = "louise";
-const nameCap =
-  name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+function capitalize(str) {
+  const name = "louise";
+  const nameCap =
+    name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
 
-console.log(`Hello my name is ${nameCap}`);
+  console.log(`Hello my name is ${nameCap}`);
+}
+
+capitalize();
